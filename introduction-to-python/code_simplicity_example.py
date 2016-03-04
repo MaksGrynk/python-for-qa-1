cc93671e5f37909854614349cfb5dbdd361937f9
yrunts <yevhenrunts@gmail.com>
2016-03-04 14:42:26 +0200

    add code simplicity example

---
+++ introduction-to-python/code_simplicity_example.py
@@ -0,0 +1,9 @@
+import urllib2
+import re
+
+url = 'https://www.facebook.com/'
+website = urllib2.urlopen(url)
+html = website.read()
+links = re.findall('"((http|ftp)s?://.*?)"', html)
+
+print links
